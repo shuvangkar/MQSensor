f4890b82f5059f1d81330db9e95357f28d544357
shuvangkar <sshuvo93@gmail.com>
2021-02-01 15:06:07 +0600

    designing new api

--- src/MQ.h
+++ src/MQ.h
@@ -2,28 +2,42 @@
 #define _MQ_H_
 #include "Arduino.h"
 
-typedef struct r0_t
+
+typedef struct calib_t
 {
+  float c;
+  float m;
+  float RL,
   float R0;
-  bool calibDone;
+  float airRsR0;
+  bool done;
 };
 
+void saveCalib(calib_t *cPtr);
+void readCalib(calib_t *cPtr);
+
+typedef void (*funCalib_t)(calib_t*);
+
+typedef void (*saveCalib)()
 class MQ
 {
   public:
     MQ(byte channel);
+    // MQ(byte ch, float RL, float airRsR0);
     void setGraphPoints(float x1, float x2, float y1, float y2);
     void setRl(float RL);
     float getPPM();
     float getRs();
     float calculateR0(float air_Rs_by_R0);
-    float Rs;
+    // float Rs;
   private:
     float _calculateRs();
     byte _analogPin;
+    float _RL;
+    float 
     float _m;
     float _c;
-    float _RL;
+    
     float _R0;
 };
 #endif
